cfd1c6f04b6a50dd55629e7dd22f889e40df2e05
illl48 <mruncanny@gmail.com>
2016-11-13 04:17:05 -0800

    change to heroku

--- app/constants.js
+++ app/constants.js
@@ -1,6 +1,6 @@
 export default {
     // MISC
-    SERVERLINK: 'https://edwardlai3582.com/',
+    SERVERLINK: 'https://edwardlai3582.herokuapp.com/',
     //"https://crossorigin.me/https://www.whosampled.com/"
     WHOSAMPLE: 'https://whosampled-illl48.c9users.io/'
 
